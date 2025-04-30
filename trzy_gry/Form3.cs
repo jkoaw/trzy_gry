cc95302bca244078178dbacdd6910a31b8f0bdae
szymon.m.olejniczak <szymon.m.olejniczak@student.put.poznan.pl>
2025-04-30 21:10:14 +0200

    zabugowane jak leci

--- trzy_gry/Form3.cs
+++ trzy_gry/Form3.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Data.SqlTypes;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
@@ -34,6 +35,8 @@ namespace trzy_gry
             Shuffle<Card>(deck);
             ShowWarArena();
             this.Click += board_Click;
+            this.Click += board_new_rozdanie_Click;
+            this.Click += board_wait_Click;
             System.Diagnostics.Debug.WriteLine(" deck = " + deck.Count);
 
         }
@@ -51,6 +54,10 @@ namespace trzy_gry
             public Label nameplate;
             public bool folded;
             public bool raised;
+
+            public HandRank handrank;
+            public int handranknumber;
+
 
             public void set(int id, string name, int height, int width, Label template)
             {
@@ -241,7 +248,7 @@ namespace trzy_gry
 
                 for (int i = 0; i < current.hand.Count; i++)
                 {
-
+                    current.hand[i].generateCardReverse();
                     current.hand[i].locationen(current.playerWidth + (arena[0][0].Width+10)*i, current.playerHeight);
                     current.hand[i].Visible = true;
                 }
@@ -297,8 +304,12 @@ namespace trzy_gry
         {
             currentRaise = 0;
             turn = 0;
+            potValue = 0;
+            factualnextturnclick = false;
             pot.Text = "pot is holding " + potValue;
             playersFolded = 0;
+            playercanclick = true;
+            waitTillNextTurn = false;
             nextturn();
         }
 
@@ -345,16 +356,294 @@ namespace trzy_gry
 
             }
         }
+
+
+        public enum HandRank
+        {
+           HighCard, OnePair, TwoPair, ThreeOfAKind,
+            Straight, Flush, FullHouse, FourOfAKind,
+            StraightFlush, RoyalFlush
+        }
+
+        
+
+        void evalute(List<Card> temp,player current)
+        {
+            
+            for (int i = 0; i < temp.Count; i++)
+            {
+                for (int j = 0; j < temp.Count; j++)
+                {
+
+                    if (temp[i].liczba < temp[j].liczba)
+                    {
+                        Card var2 = temp[i];
+                        temp[i] = temp[j];
+                        temp[j] = var2;
+                    }
+                }
+            }
+            for (int i = 0; i < temp.Count; i++)
+            {
+                System.Diagnostics.Debug.WriteLine("winner " + temp[i].liczba);
+            }
+
+
+            //Royal Flush------------------------------------------------------------
+            int licznik = 0;
+            for (int i = 0; i < temp.Count-1; i++)
+            {
+                if (14 == temp[i].liczba)
+                {
+                    if (temp[i].liczba == temp[i + 1].liczba + 1 && temp[i].znaczek == temp[i + 1].znaczek)
+                    {
+                        licznik++;
+
+
+                    }
+                }
+                else break;
+             
+            }
+            if(licznik >= 5) { current.handrank = HandRank.RoyalFlush; return; }
+            licznik = 0;
+            //Straight Flush------------------------------------------------------------
+            for (int i = 0; i < temp.Count - 1; i++)
+            {              
+                if (temp[i].liczba == temp[i + 1].liczba + 1 && temp[i].znaczek == temp[i + 1].znaczek)
+                {
+                    licznik++;
+                }
+                else break;
+
+            }
+            if (licznik >= 5) { current.handrank = HandRank.StraightFlush; current.handranknumber = temp[0].liczba; return; }
+            //Four of a Kind------------------------------------------------------------
+            for (int i = 0; i < temp.Count - 1; i++)
+            {
+                if (temp[i].liczba == temp[i + 1].liczba )
+                {
+                    licznik++;
+                }
+                
+            }
+            if (licznik == 4) { current.handrank = HandRank.FourOfAKind; current.handranknumber = temp[0].liczba; return; }
+            licznik = 0;
+            int licznik2 = 0;
+            int wynik = 0;
+            //Full House------------------------------------------------------------
+            int var = 0;
+            for (int i = 0; i < temp.Count - 1; i++)
+            {
+                if (temp[i].liczba == temp[i + 1].liczba && licznik2==0) //jezeli drugapara lub 3 jeszcze nie policzona
+                {
+                    var = i;
+                    licznik++;
+                    wynik += temp[i].liczba;
+                }
+                if (temp[i].liczba == temp[i + 1].liczba && temp[var].liczba != temp[i].liczba)
+                {
+                    licznik2++;
+                    wynik += temp[i].liczba;
+                }
+               
+            }
+            
+            if ((licznik == 3 || licznik >= 1) && (licznik2 == 3 || licznik2 >= 1)) { current.handrank = HandRank.FullHouse; current.handranknumber = wynik; return; } //mabe player extension maybe 1 lub  2 ten wynik uhh
+            licznik = 0;
+            var = 0;
+            licznik2 = 0;
+            wynik = 0;
+            //Flush------------------------------------------------------------
+            for (int i = 0; i < temp.Count - 1; i++)
+            {
+                if (temp[i].znaczek == temp[i + 1].znaczek ) 
+                {
+                    licznik++;
+                    wynik += temp[i].liczba;
+                }
+               
+            }
+            
+            if ((licznik >= 5 ) ) { current.handrank = HandRank.Flush; current.handranknumber = wynik; return; } 
+            licznik = 0;
+            licznik2 = 0;
+            wynik = 0;
+            //Straight------------------------------------------------------------
+            for (int i = 0; i < temp.Count - 1; i++)
+            {
+                if (temp[i].liczba == temp[i + 1].liczba +1) 
+                {
+                    licznik++;
+                    wynik += temp[i].liczba;
+                }
+
+            }
+            
+            if ((licznik >= 5)) { current.handrank = HandRank.Straight; current.handranknumber = wynik; return; } 
+            licznik = 0;
+            licznik2 = 0;
+            wynik = 0;
+            // Three of a Kind------------------------------------------------------------ bugg zał liczba dwie 6 jako i w dodatku krówl xd
+            for (int i = 0; i < temp.Count - 1; i++)
+            {
+                if (temp[i].liczba == temp[i + 1].liczba ) 
+                {
+                    licznik++;
+                    wynik = temp[i].liczba;
+                }
+
+            }
+
+            if ((licznik >= 3)) { current.handrank = HandRank.ThreeOfAKind; current.handranknumber = wynik; return; } 
+            licznik = 0;
+            licznik2 = 0;
+            wynik = 0;
+            // Two Pair------------------------------------------------------------
+            for (int i = 0; i < temp.Count - 1; i++)
+            {
+                if (temp[i].liczba == temp[i + 1].liczba && licznik2 == 0) 
+                {
+                    var = i;
+                    licznik++;
+                    wynik += temp[i].liczba;
+                }
+                if (temp[i].liczba == temp[i + 1].liczba && temp[var].liczba != temp[i].liczba)
+                {
+                    licznik2++;
+                    wynik += temp[i].liczba;
+                }
+
+            }
+
+            if ((licznik == 1) && ( licznik2 >= 1)) { current.handrank = HandRank.TwoPair; current.handranknumber = wynik; return; } 
+            licznik = 0;
+            licznik2 = 0;
+            wynik = 0;
+            // One Pair------------------------------------------------------------
+            for (int i = 0; i < temp.Count - 1; i++)
+            {
+                if (temp[i].liczba == temp[i + 1].liczba)
+                {
+                   
+                    licznik++;
+                    wynik += temp[i].liczba;
+                }
+               
+
+            }
+
+            if ((licznik == 1)) { current.handrank = HandRank.OnePair; current.handranknumber = wynik; return; }
+            licznik = 0;
+            licznik2 = 0;
+            wynik = 0;
+
+            // One Pair------------------------------------------------------------
+            current.handrank = HandRank.HighCard; current.handranknumber = temp[0].liczba; return;
+
+            // 3 te same i 2 nie wykrywa 3+2 pot trza naprawic dodawanie do raczej xdD
+        }
+
+        void whoWon()
+        {
+            
+            int winner = -1;
+            List<int> tie = new List<int> { };
+            for (int i = 0; i < players.Count; i++)
+            {
+                if (players[i].folded) continue;
+                if (winner == -1) { winner = i; continue; }
+                
+
+                if (players[i].handrank == players[winner].handrank)
+                {
+                    if(players[i].handranknumber > players[winner].handranknumber)
+                    {
+                        winner = i;
+                    }
+                    if (players[i].handranknumber == players[winner].handranknumber)
+                    {
+
+                        tie.Add(winner);
+                        tie.Add(i);
+                    }
+                }
+
+
+                if (players[i].handrank > players[winner].handrank)
+                {
+                    winner = i;
+                }
+                
+            }
+
+            if (tie.Count >0)
+            {
+                pot.Text = " tie  with";
+                foreach (int item in tie)
+                {
+                    players[item].money += (int)(potValue / tie.Count); // wiem że są krajne przypadki albo nie nie wiem
+                    pot.Text +=  " player " + players[winner].id;
+                }
+            }
+            else
+            {
+                players[winner].money = potValue;
+                pot.Text = " winner  player" + players[winner].id;
+            }
+            System.Diagnostics.Debug.WriteLine("winner " + winner);
+
+        }
+
+        bool waitTillNextTurn = false;
+
+        void showCards(int var)
+        {
+            System.Diagnostics.Debug.WriteLine("player[" + var + "] liczba = " + players[var].handrank + " wartosc = " + players[var].handranknumber);
+            for (int i = 0; i < players[var].hand.Count; i++)
+            {
+                players[var].hand[i].generateCard();
+                
+            }
+            
+            
+        }
+
         void decideWinner()
         {
-            pot.Text = "decide winner";
+            List<Card> temp = new List<Card> { };
+
+            for (int i = 0; i < players.Count; i++)
+            {
+                if (players[i].folded) continue;
+
+                
+                for (int j = 0; j < players[i].hand.Count; j++)
+                {
+                    temp.Add(players[i].hand[j]);
+                }
+                for (int j = 0; j < arena.Count; j++)
+                {
+                    temp.Add(arena[j][0]);
+                }
+                evalute(temp, players[i]);
+                showCards(i);
+                temp.Clear();
+            }
+            whoWon();
+            
+            waitTillNextTurn = true;
         }
+
+
+
+
         int playersFolded = 0;
         void nextturn()
         {
 
             if (turn == 3) { System.Diagnostics.Debug.WriteLine("turn 3  decide Winner" ); decideWinner(); }
-            if(playersFolded > 3 ) { System.Diagnostics.Debug.WriteLine("folded > 3  decide Winner"); decideWinner(); }
+            if(playersFolded >= 3 ) { System.Diagnostics.Debug.WriteLine("folded > 3  decide Winner"); decideWinner(); }
 
             if (turn == 0)
             {
@@ -465,7 +754,7 @@ namespace trzy_gry
         {
             messege.Text = "";
             if (playercanclick) risefun(players[players.Count - 1]);
-                 playercanclick = false;
+            playercanclick = false;
         }
 
         private void check_Click(object sender, EventArgs e)
@@ -476,6 +765,7 @@ namespace trzy_gry
         }
         private void board_Click(object sender, EventArgs e)
         {
+            if (waitTillNextTurn) return;
             for (int i = 0; i < players.Count; i++)
             {
              //   System.Diagnostics.Debug.WriteLine("player [" + i + "] money = " + players[i].money);
@@ -492,6 +782,59 @@ namespace trzy_gry
                 nextturn();
 
             }
+        }
+        private void board_wait_Click(object sender, EventArgs e)
+        {
+            if (!waitTillNextTurn) return;
+            factualnextturnclick = true;
+        }
+
+        bool factualnextturnclick = false;
+        private void board_new_rozdanie_Click(object sender, EventArgs e)
+        {
+            if (!factualnextturnclick) return;
+            
+            System.Diagnostics.Debug.WriteLine("nowe rozdanie");
+
+            //resetownie rąk
+
+            for (int i = 0; i < players.Count ; i++)
+            {
+
+                players[i].hand.Clear();
+            }
+            for (int i = 0; i < arena.Count; i++)
+            {
+
+                arena[i].Clear();
+            }
+
+            for (int i = 0; i < deck.Count; i++)
+            {
+                deck[i].Visible = false;
+            }
+
+            Shuffle<Card>(deck);
+            Shuffle<Card>(deck);
+
+
+            ShowWarArena();
+            for (int i = 5; i < 5 + players.Count * 2; i++)
+            {
+               
+                players[(i - 5) % 4].hand.Add(deck[i + 10]);
+            }
+     
+            for (int i = 0; i < 4; i++)
+            {
+                
+                spawnhand(players[i]);
+                //System.Diagnostics.Debug.WriteLine("player [" + i + "] hand = "+ players[i].hand.Count);
+            }
+
+            newGameHandler();
+
+
         }
         private void Form3_Load(object sender, EventArgs e)
         {
